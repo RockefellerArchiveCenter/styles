558fb4c32d2d520c5e60e585a8d785d3754fb406
Hannah <hsistrunk@gmail.com>
2024-08-30 13:35:27 -0400

    updates to fix scss color variable export/import

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,41 +1,56 @@
-const config = {
+export default {
+  framework: {
+    name: '@storybook/html-webpack5',
+    options: {}
+  },
   stories: [
     '../stories/**/*.mdx',
     '../stories/**/*.stories.@(js|jsx|ts|tsx)'
   ],
 
-  addons: ['@storybook/addon-docs', {
-    name: '@storybook/addon-essentials',
-    options: {
-      actions: false
-    }
-  }, '@whitespace/storybook-addon-html', '@storybook/addon-links', '@storybook/addon-a11y', {
-    name: '@storybook/preset-scss',
-    options: {
-      cssLoaderOptions: {
-        modules: {
-          mode: 'icss' // allows use of :export directive
-        }
+  addons: [
+    '@storybook/addon-docs',
+    '@whitespace/storybook-addon-html',
+    '@storybook/addon-links',
+    '@storybook/addon-a11y',
+    '@storybook/addon-webpack5-compiler-swc',
+    {
+      name: '@storybook/addon-essentials',
+      options: {
+        actions: false
       }
+    },
+    {
+      name: '@storybook/addon-styling-webpack',
+      options: {
+        rules: [
+          {
+            test: /\.s[ac]ss$/i,
+            use: [
+              'style-loader',
+              {
+                loader: 'css-loader',
+                options: {
+                  modules: {
+                    mode: 'icss' // allows use of :export directive
+                  },
+                },
+              },
+              'sass-loader',
+            ],
+          },
+        ],
+      },
     }
-  }, '@storybook/addon-webpack5-compiler-swc'],
+  ],
 
-  webpackFinal: async (config, { configType }) => {
-    // `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
-    // You can change the configuration based on that.
-    // 'PRODUCTION' is used when building the static version of storybook.
-
+  webpackFinal: async (config) => {
     // Load Handlebars templates
     config.module.rules.push({
       test: /\.handlebars$/,
       use: ['handlebars-loader']
-    })
-    return config
-  },
+    });
 
-  framework: {
-    name: '@storybook/html-webpack5',
-    options: {}
-  }
+    return config;
+  },
 };
-export default config;
